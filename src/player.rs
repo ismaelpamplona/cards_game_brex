eddc2634862fffddf376871b14788f11f8b8678f
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-06-07 16:45:49 -0300

    (up) Issue #implement_player_struct.

--- src/player.rs
+++ src/player.rs
@@ -38,3 +38,94 @@ impl Player {
         true
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::card::CardValue;
+
+    #[test]
+    fn test_can_purchase_success() {
+        let mut player = Player::new();
+        player.wallet.insert(Color::R, 5);
+        player.wallet.insert(Color::G, 3);
+        player.wallet.insert(Color::B, 4);
+
+        let mut card_value = CardValue::new();
+        card_value.add_gem(Color::R, 1);
+        card_value.add_gem(Color::G, 2);
+        card_value.add_gem(Color::B, 3);
+
+        let card = Card {
+            color: Color::G,
+            value: card_value,
+        };
+
+        assert!(player.can_purchase(&card));
+    }
+
+    #[test]
+    fn test_can_purchase_failure() {
+        let mut player = Player::new();
+        player.wallet.insert(Color::R, 0);
+        player.wallet.insert(Color::G, 1);
+        player.wallet.insert(Color::B, 1);
+
+        let mut card_value = CardValue::new();
+        card_value.add_gem(Color::R, 1);
+        card_value.add_gem(Color::G, 2);
+        card_value.add_gem(Color::B, 3);
+
+        let card = Card {
+            color: Color::G,
+            value: card_value,
+        };
+
+        assert!(!player.can_purchase(&card));
+    }
+
+    #[test]
+    fn test_purchase_success() {
+        let mut player = Player::new();
+        player.wallet.insert(Color::R, 5);
+        player.wallet.insert(Color::G, 3);
+        player.wallet.insert(Color::B, 4);
+
+        let mut card_value = CardValue::new();
+        card_value.add_gem(Color::R, 1);
+        card_value.add_gem(Color::G, 2);
+        card_value.add_gem(Color::B, 3);
+
+        let card = Card {
+            color: Color::G,
+            value: card_value,
+        };
+
+        assert!(player.purchase(card));
+        assert_eq!(player.wallet.get(&Color::R), Some(&4));
+        assert_eq!(player.wallet.get(&Color::G), Some(&1));
+        assert_eq!(player.wallet.get(&Color::B), Some(&1));
+        assert_eq!(player.deck.len(), 1);
+    }
+
+    #[test]
+    fn test_purchase_failure() {
+        let mut player = Player::new();
+        player.wallet.insert(Color::R, 0);
+        player.wallet.insert(Color::G, 1);
+        player.wallet.insert(Color::B, 1);
+
+        let mut card_value = CardValue::new();
+        card_value.add_gem(Color::R, 1);
+        card_value.add_gem(Color::G, 2);
+        card_value.add_gem(Color::B, 3);
+
+        let card = Card {
+            color: Color::G,
+            value: card_value,
+        };
+
+        assert!(!player.purchase(card));
+        assert_eq!(player.deck.len(), 0);
+    }
+}
