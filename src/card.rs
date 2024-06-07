ffccef51a9da5f2214615a27dece0c00390b095d
Ismael Pamplona <ismaelpamplona@hotmail.com>
2024-06-07 16:45:40 -0300

    (up) Issue #implement_card_struct.

--- src/card.rs
+++ src/card.rs
@@ -27,3 +27,56 @@ impl CardValue {
         *self.gems.get(color).unwrap_or(&0)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::color::Color;
+
+    #[test]
+    fn test_card_value_new() {
+        let card_value = CardValue::new();
+        assert!(card_value.gems.is_empty());
+    }
+
+    #[test]
+    fn test_card_value_add_gem() {
+        let mut card_value = CardValue::new();
+        card_value.add_gem(Color::R, 5);
+        assert_eq!(card_value.get_gem(&Color::R), 5);
+    }
+
+    #[test]
+    fn test_card_value_get_gem_default() {
+        let card_value = CardValue::new();
+        assert_eq!(card_value.get_gem(&Color::R), 0);
+    }
+
+    #[test]
+    fn test_card_value_get_gem_after_add() {
+        let mut card_value = CardValue::new();
+        card_value.add_gem(Color::R, 5);
+        card_value.add_gem(Color::G, 3);
+        assert_eq!(card_value.get_gem(&Color::R), 5);
+        assert_eq!(card_value.get_gem(&Color::G), 3);
+        assert_eq!(card_value.get_gem(&Color::B), 0);
+    }
+
+    #[test]
+    fn test_card_creation() {
+        let mut card_value = CardValue::new();
+        card_value.add_gem(Color::R, 1);
+        card_value.add_gem(Color::G, 2);
+        card_value.add_gem(Color::B, 3);
+
+        let card = Card {
+            color: Color::G,
+            value: card_value,
+        };
+
+        assert_eq!(card.color, Color::G);
+        assert_eq!(card.value.get_gem(&Color::R), 1);
+        assert_eq!(card.value.get_gem(&Color::G), 2);
+        assert_eq!(card.value.get_gem(&Color::B), 3);
+    }
+}
